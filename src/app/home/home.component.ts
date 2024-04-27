2e33428e59b6fbd93577f6fdf80658ec1acb5361
Augustine Normanyo <augustine.normanyo@amalitech.com>
2024-04-27 20:40:24 +0100

    new app creation setup updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,60 +2,104 @@ import { Component } from '@angular/core';
 import { InputFieldComponent } from '../reusable/input-field/input-field.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
+import { SelectFieldComponent } from '../reusable/select-field/select-field.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [InputFieldComponent, CommonModule, FormsModule],
+  imports: [InputFieldComponent, SelectFieldComponent, CommonModule, FormsModule],
   template: `
     <div class="w-full h-screen border">
-      <app-input-field
-        [type]="'text'"
-        [style]="''"
-        [placeholder]="'Enter your firstname here...'"
-        [required]="false"
-        [label]="'firstname'"
-        [identifier]="'firstname'"
-        [model]="firstname"
-        (modelChange)="onInputChange($event)"
-      ></app-input-field>
+      <div *ngFor="let val of fields; let idx = index">
+        <ng-container *ngIf="idx < 10">
+          <app-input-field
+            [type]="'text'"
+            [style]="''"
+            [placeholder]="placeholderData[idx]"
+            [required]="false"
+            [label]="labelData[idx]"
+            [identifier]="identifyData[idx]"
+            [model]="modelvalues[idx]"
+            (modelChange)="onInputChange($event)"
+          ></app-input-field>
+        </ng-container>
 
-      <app-input-field
-        [type]="'text'"
-        [style]="''"
-        [placeholder]="'Enter your lastname here...'"
-        [required]="false"
-        [label]="'lastname'"
-        [identifier]="'lastname'"
-        [model]="lastname"
-        (modelChange)="onInputChange($event)"
-      ></app-input-field>
+      </div>
 
-      <button (click)="click()">click</button>
+      <div>
+       <app-select-field 
+        [data]="[
+          {itemId:1, itemName:'Male'},
+          {itemId:2, itemName:'Female'}
+        ]"
+        [model]="selectModel"
+        (modelChange)="handleSelect($event)"
+        (selectChange)="onSelected($event)"
+       />
+      </div>
+
+      <button (click)="click()" class="w-[10%] p-2 rounded border">
+        click
+      </button>
+      <button (click)="onEdit('augustine', 'normanyo', '1')" class="w-[10%] p-2 rounded border ml-1">
+        edit
+      </button>
     </div>
   `,
 })
 export class HomeComponent {
-  String(arg0: any): string {
-    throw new Error('Method not implemented.');
-  }
-  firstname = '';
-  lastname = '';
+  fields = new Array(2);
+  placeholderData = ['Enter firstname', 'Enter lastname'];
+  labelData = ['Firstname', 'Lastname'];
+  identifyData = ['firstname', 'lastname'];
+
+  modelvalues = ['', ''];
+  selectModel = ''
+  selctobject:{ itemId: number; itemName: string } = {
+    itemId: 0,
+    itemName: ''
+  } 
 
   constructor() {}
 
   click() {
-    console.log(this.firstname, this.lastname);
+    console.log({
+      'firstname': this.modelvalues[0],
+      'lastname:': this.modelvalues[1],
+      'select': this.selectModel,
+      'selectoptions': this.selctobject
+    });
+    this.reset();
   }
 
   onInputChange(event: { value: string; identifier: string }) {
     if (event.identifier === 'firstname') {
-      this.firstname = event.value;
-      console.log(this.firstname)
+      this.modelvalues[0] = event.value;
     } else if (event.identifier === 'lastname') {
-      this.lastname = event.value;
-      console.log(this.lastname)
+      this.modelvalues[1] = event.value;
     }
+  }
+
+  handleSelect(event:string){
+    let value = event
+    this.selectModel = value;
+  }
+
+  onSelected(event:{ itemId: number; itemName: string }){
+    this.selctobject = event
+  }
+
+  //simulate edit
+  onEdit(firstname:string, lastname:string, gender:string){
+    this.modelvalues[0] = firstname;
+    this.modelvalues[1]  = lastname;
+    this.selectModel = gender;
+  }
+
+  reset(){
+    this.modelvalues.forEach((_, index) => {
+      this.modelvalues[index] = '';
+    });
+    this.selectModel = '';
   }
 
   ngOnInit() {}
--- src/app/reusable/select-field/select-field.component.ts
+++ src/app/reusable/select-field/select-field.component.ts
@@ -1,13 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-select-field',
   standalone: true,
-  imports: [],
-  template:`
-   <div></div>
-  `
+  imports: [CommonModule, FormsModule],
+  template: `
+    <div class="w-full p-2">
+      <div class="flex justify-between mb-2">
+        <div class="w-1/2 text-start font-bold">{{ label }}</div>
+        <div class="w-1/2 text-red-500 text-end font-bold" *ngIf="required">
+          Required
+        </div>
+      </div>
+      <div>
+        <select
+          [class]="
+            'w-full p-2 bg-white border border-[#d8dae5] hover:border-[#8F95B2] text-gray-900 rounded-lg focus:outline-cyan-200 focus:ring-cyan-500 focus:ring placeholder-option ' +
+            ' ' +
+            style
+          "
+          [ngClass]="{ 'placeholder-option': !model }"
+          [(ngModel)]="model"
+          (ngModelChange)="onInputChange(model)"
+          (ngModelChange)="onSelectChange($event)"
+        >
+          <option class="placeholder-option"  value="" disabled>{{ placeholder }}</option>
+          <option
+            *ngFor="let val of data; let idx = index"
+            [value]="val.itemId"
+            
+          >
+            {{ val.itemName }}
+          </option>
+        </select>
+      </div>
+    </div>
+  `,
 })
 export class SelectFieldComponent {
+  @Input() style!: string;
+  @Input() label!: string;
+  @Input() required!: boolean;
+  @Input() placeholder: string = 'somthin here';
+  @Input() data: { itemId: number; itemName: string }[] = [];
+  @Input() model!: string;
+  @Output() modelChange = new EventEmitter<string>();
+  @Output() selectChange = new EventEmitter<{
+    itemId: number;
+    itemName: string;
+  }>();
 
+  onInputChange($event: string) {
+    this.modelChange.emit($event);
+  }
+  onSelectChange(event: { itemId: number; itemName: string } | undefined) {
+    this.selectChange.emit(event);
+  }
+
+  //lifecycle hook to keep track of data
+  ngOnChanges(changes: any) {
+    if (changes.model && this.data) {
+      let find = this.data.find(
+        (el) => el.itemId == changes.model.currentValue
+      );
+      this.onSelectChange(find);
+    }
+  }
 }
